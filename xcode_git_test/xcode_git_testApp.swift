f105aaed76ac3848580994b982820e909e8fa3f5
Julie <ssabini.sl@gmail.com>
2022-03-28 10:19:20 +0900

    Initial Commit

---
+++ xcode_git_test/xcode_git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_git_testApp.swift
+//  xcode_git_test
+//
+//  Created by Julie on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct xcode_git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
